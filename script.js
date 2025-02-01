8b4679dad4ea5e6c3a07f83f1d5d4affd3e64408
Gaurav Kumar Singh <gsingh332211@gmail.com>
2025-02-01 22:35:25 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-// Smooth Scrolling
-// GSAP
-// Scroll Trigger
+function circleMouseFollower() {
+  const cursor = document.querySelector(".cursor");
+  cursor.style.opacity = 0;
 
-// function cursor() {
-//   window.addEventListener("mousemove", function (dets) {
-//     console.log(dets);
-//   });
-// }
-// cursor();
+  window.addEventListener("load", () => {
+    cursor.style.opacity = 1;
+  });
+
+  window.addEventListener("mousemove", (dets) => {
+    // Using requestAnimationFrame for smooth animation
+    requestAnimationFrame(() => {
+      cursor.style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) translate(-50%, -50%)`;
+    });
+  });
+  window.addEventListener("mouseout", () => {
+    cursor.style.opacity = 0;
+  });
+
+  window.addEventListener("mouseover", () => {
+    cursor.style.opacity = 1;
+  });
+}
 
 const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
+circleMouseFollower();
