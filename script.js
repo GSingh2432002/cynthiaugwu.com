ea60d0ffe70f68d6078b81203e9d8bf6c4a6ccc9
Gaurav Kumar Singh <gsingh332211@gmail.com>
2025-02-03 23:32:39 +0530

    final commit, not yet responsive

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
 var timeout;
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
-});
+
+function introduction() {
+  document.addEventListener("DOMContentLoaded", function () {
+    let timerElement = document.querySelector(".timer");
+    let introPage = document.querySelector(".intro-page");
+    let progress = 0;
+    let interval = setInterval(() => {
+      progress += 5; // Increase percentage step
+      timerElement.textContent = `${progress}%`;
+
+      if (progress >= 100) {
+        clearInterval(interval);
+        // Hide the intro page and show main content
+        introPage.style.opacity = "0";
+        setTimeout(() => {
+          introPage.style.display = "none";
+        }, 500);
+      }
+    }, 100);
+  });
+}
+introduction();
+
+// For Showing Current timing:-
+setInterval(setTime, 100);
+function setTime() {
+  var NewDate = new Date();
+  var formattedTime = NewDate.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  let time = document.querySelectorAll(".current-time");
+  time.forEach((e) => {
+    e.innerHTML = formattedTime + " IST";
+  });
+}
+
+setTimeout(() => {
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+}, 1000);
 
 function firstPageAnimation() {
   var tl1 = gsap.timeline();
@@ -88,19 +127,63 @@ function circleMouseFollower() {
 
 // Handle cursor tracking & visibility.
 circleMouseFollower();
-
 // Adds the scaling effect based on movement speed.
 circleScaling();
-
 // Runs GSAP animations whenever the page loads.
 firstPageAnimation();
 
 // Animation for Second Page Only.
 document.querySelectorAll(".element").forEach(function (element) {
+  var rotate = 0;
+  var diffRotate = 0;
+
+  element.addEventListener("mouseleave", function (dets) {
+    gsap.to(element.querySelector("a"), {
+      opacity: 0.7,
+      duration: 0.3,
+      ease: Power3,
+      x: 0,
+    });
+
+    gsap.to(element.querySelector(".year"), {
+      opacity: 0.7,
+      duration: 0.3,
+      ease: Power3,
+    });
+
+    gsap.to(element.querySelector("img"), {
+      opacity: 0,
+      ease: Power3,
+      duration: 0.3,
+      delay: 0.2,
+    });
+  });
+
   element.addEventListener("mousemove", function (dets) {
+    var diff = dets.clientY - element.getBoundingClientRect().top;
+    diffRotate = dets.clientX - rotate;
+    rotate = dets.clientX;
+    gsap.to(element.querySelector("a"), {
+      opacity: 0.3, // Adjust this value as needed
+      duration: 0.3,
+      ease: Power3,
+      x: 20,
+    });
+
+    gsap.to(element.querySelector(".year"), {
+      opacity: 0.3,
+      duration: 0.3,
+      ease: Power3,
+    });
+
     gsap.to(element.querySelector("img"), {
       opacity: 1,
-      ease: Power1,
+      borderRadius: "5%",
+      ease: Power3,
+      top: diff,
+      left: dets.clientX,
+
+      rotate: gsap.utils.clamp(-20, 20, diffRotate * 0.5),
     });
   });
 });
